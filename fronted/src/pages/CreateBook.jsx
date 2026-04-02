081d04d29528c219d3301cecec09e6726e1d349f
Nikita Satish Madke <madkenikita35@gmail.com>
2026-04-02 22:38:50 +0530

    Add Changes

--- fronted/src/pages/CreateBook.jsx
+++ fronted/src/pages/CreateBook.jsx
@@ -31,9 +31,9 @@ const CreateBook = () => {
   };
 
   return (
-    <div className="p-4 ">
+    <div className="p-4">
       <BackButton />
-      <h1 className="text-3xl my-4">Create Book </h1>
+      <h1 className="text-3xl my-4 ">Create Book </h1>
       {loading ? <Spinner /> : ""}
       <div className=" flex flex-col border border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
         <div className="my-4">
