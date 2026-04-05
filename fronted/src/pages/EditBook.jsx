68604aa4b1358ce4652851d8b839f9dbf15162a2
Nikita Satish Madke <madkenikita35@gmail.com>
2026-04-05 23:40:50 +0530

    Add Styling

--- fronted/src/pages/EditBook.jsx
+++ fronted/src/pages/EditBook.jsx
@@ -50,9 +50,9 @@ const EditBook = () => {
   };
 
   return (
-    <div className=" p-4 ">
+    <div className="p-4">
       <BackButton />
-      <h1 className="text-3xl my-4">Edit Book</h1>
+      <h1 className="text-3xl my-4 ">Edit Book</h1>
       {loading ? <Spinner /> : ""}
       <div className="flex flex-col border border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
         <div className="my-4">
