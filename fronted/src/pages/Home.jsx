1b0bc350f3b76f7e2676450f3f3053bbc56907b9
Nikita Satish Madke <madkenikita35@gmail.com>
2026-04-10 00:42:46 +0530

    Add Style

--- fronted/src/pages/Home.jsx
+++ fronted/src/pages/Home.jsx
@@ -47,7 +47,7 @@ const Home = () => {
           Table
         </button>
         <button
-          className=" bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
           onClick={() => setShowType("card")}
         >
           Card
