53746e029488c91448f8fc8c952e3bac1f6d9e89
Nikita Satish Madke <madkenikita35@gmail.com>
2026-04-23 23:26:34 +0530

    Add Styling Changes

--- fronted/src/pages/Home.jsx
+++ fronted/src/pages/Home.jsx
@@ -41,20 +41,20 @@ const Home = () => {
     >
       <div className="flex justify-center items-center gap-x-4 ">
         <button
-          className=" bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
           onClick={() => setShowType("table")}
         >
           Table
         </button>
         <button
-          className=" bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
           onClick={() => setShowType("card")}
         >
           Card
         </button>
       </div>
 
-      <div className=" flex justify-between items-center ">
+      <div className=" flex justify-between items-center">
         <h1 className="text-3xl my-8">Book List</h1>
         <Link to="/books/create">
           <MdOutlineAddBox className="text-sky-800 text-4xl" />
