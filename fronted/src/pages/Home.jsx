a57e64bcda8a8c55cd4910a02da51390ce43ef5a
Nikita Satish Madke <madkenikita35@gmail.com>
2026-04-23 23:25:42 +0530

    Add Changes

--- fronted/src/pages/Home.jsx
+++ fronted/src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
     >
       <div className="flex justify-center items-center gap-x-4 ">
         <button
-          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
+          className=" bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
           onClick={() => setShowType("table")}
         >
           Table
@@ -57,7 +57,7 @@ const Home = () => {
       <div className=" flex justify-between items-center">
         <h1 className="text-3xl my-8">Book List</h1>
         <Link to="/books/create">
-          <MdOutlineAddBox className="text-sky-800 text-4xl " />
+          <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
       </div>
       {loading ? (
