5e81c0bf419abdcc71707bb54dfa83eba1ec0336
Nikita Satish Madke <madkenikita35@gmail.com>
2026-04-25 23:05:17 +0530

    Add Changes

--- fronted/src/pages/Home.jsx
+++ fronted/src/pages/Home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
       <div className=" flex justify-between items-center">
         <h1 className="text-3xl my-8">Book List</h1>
         <Link to="/books/create">
-          <MdOutlineAddBox className="text-sky-800 text-4xl" />
+          <MdOutlineAddBox className="text-sky-800 text-4xl " />
         </Link>
       </div>
       {loading ? (
