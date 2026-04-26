ae1a976489b818e48d29b522b2a6a06783eb882d
Nikita Satish Madke <madkenikita35@gmail.com>
2026-04-26 22:44:03 +0530

    Add

--- fronted/src/pages/Home.jsx
+++ fronted/src/pages/Home.jsx
@@ -39,9 +39,9 @@ const Home = () => {
       bg-cover
       bg-center
     >
-      <div className="flex justify-center items-center gap-x-4 ">
+      <div className="flex justify-center items-center gap-x-4">
         <button
-          className=" bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g "
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
           onClick={() => setShowType("table")}
         >
           Table
@@ -54,7 +54,7 @@ const Home = () => {
         </button>
       </div>
 
-      <div className=" flex justify-between items-center">
+      <div className="flex justify-between items-center">
         <h1 className="text-3xl my-8">Book List</h1>
         <Link to="/books/create">
           <MdOutlineAddBox className="text-sky-800 text-4xl " />
@@ -62,7 +62,7 @@ const Home = () => {
       </div>
       {loading ? (
         <Spinner />
-      ) : ShowType === "table" ? (
+      ) : ShowType === "table " ? (
         <BooksTable books={books} />
       ) : (
         <BooksCard books={books} />
