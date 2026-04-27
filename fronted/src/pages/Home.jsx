b360f1b58d2f2cd51d1aae075ada094d3d213f4e
Nikita Satish Madke <madkenikita35@gmail.com>
2026-04-27 21:40:31 +0530

    Add Chnages

--- fronted/src/pages/Home.jsx
+++ fronted/src/pages/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
       bg-cover
       bg-center
     >
-      <div className=" flex justify-center items-center gap-x-4">
+      <div className="flex justify-center items-center gap-x-4">
         <button
           className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
           onClick={() => setShowType("table")}
