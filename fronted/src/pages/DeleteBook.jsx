3a86dc99cf06cddd5865f094d9a99370b8fd313c
Nikita Satish Madke <madkenikita35@gmail.com>
2026-05-01 23:54:36 +0530

    Add Changes

--- fronted/src/pages/DeleteBook.jsx
+++ fronted/src/pages/DeleteBook.jsx
@@ -25,7 +25,7 @@ const DeleteBook = () => {
   return (
     <div className=" p-4">
       <BackButton />
-      <h1 className="text-3xl my-4 ">Delete Book</h1>
+      <h1 className="text-3xl my-4">Delete Book</h1>
       {loading ? <Spinner /> : ""}
       <div className="flex flex-col item-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto ">
         <h3 className="text-2xl">
