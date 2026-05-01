25fcf44dffef75bfbb542b90b0a063a5fff64d00
Nikita Satish Madke <madkenikita35@gmail.com>
2026-05-01 23:54:44 +0530

    Add Chnaegs

--- fronted/src/pages/Home.jsx
+++ fronted/src/pages/Home.jsx
@@ -1,74 +1,74 @@
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// import Spinner from "../components/Spinner";
-// import { Link } from "react-router-dom";
-// import { AiOutlineEdit } from "react-icons/ai";
-// import { BsInfoCircle } from "react-icons/bs";
-// import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
-// import BooksCard from "../components/home/BooksCard.jsx";
-// import BooksTable from "../components/home/BooksTable.jsx";
-// import bg from "../assets/bg.jpg";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import { Link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import BooksCard from "../components/home/BooksCard.jsx";
+import BooksTable from "../components/home/BooksTable.jsx";
+import bg from "../assets/bg.jpg";
 
-// const Home = () => {
-//   const [books, setBooks] = useState([]);
-//   const [loading, setLoading] = useState(false);
-//   const [ShowType, setShowType] = useState("table");
+const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [ShowType, setShowType] = useState("table");
 
-//   useEffect(() => {
-//     setLoading(true);
-//     axios
-//       .get("http://localhost:5555/books")
-//       .then((res) => {
-//         setBooks(res.data.data);
-//         setLoading(false);
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//         setLoading(false);
-//       });
-//   }, []);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:5555/books")
+      .then((res) => {
+        setBooks(res.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
 
-//   return (
-//     <div
-//       className="p-4"
-//       style={{
-//         backgroundImage: `url(${bg})`,
-//         backgroundSize: " cover ",
-//         backgroundPosition: " center ",
-//       }}
-//       bg-cover
-//       bg-center
-//     >
-//       <div className="flex justify-center items-center gap-x-4">
-//         <button
-//           className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
-//           onClick={() => setShowType("table")}
-//         >
-//           Table
-//         </button>
-//         <button
-//           className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
-//           onClick={() => setShowType("card")}
-//         >
-//           Card
-//         </button>
-//       </div>
+  return (
+    <div
+      className="p-4"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: " cover ",
+        backgroundPosition: " center ",
+      }}
+      bg-cover
+      bg-center
+    >
+      <div className="flex justify-center items-center gap-x-4">
+        <button
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
+          onClick={() => setShowType("table")}
+        >
+          Table
+        </button>
+        <button
+          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
+          onClick={() => setShowType("card")}
+        >
+          Card
+        </button>
+      </div>
 
-//       <div className="flex justify-between items-center">
-//         <h1 className=" text-3xl my-8 ">Book List</h1>
-//         <Link to="/books/create">
-//           <MdOutlineAddBox className="text-sky-800 text-4xl " />
-//         </Link>
-//       </div>
-//       {loading ? (
-//         <Spinner />
-//       ) : ShowType === "table " ? (
-//         <BooksTable books={books} />
-//       ) : (
-//         <BooksCard books={books} />
-//       )}
-//     </div>
-//   );
-// };
+      <div className="flex justify-between items-center">
+        <h1 className=" text-3xl my-8 ">Book List</h1>
+        <Link to="/books/create">
+          <MdOutlineAddBox className="text-sky-800 text-4xl " />
+        </Link>
+      </div>
+      {loading ? (
+        <Spinner />
+      ) : ShowType === "table " ? (
+        <BooksTable books={books} />
+      ) : (
+        <BooksCard books={books} />
+      )}
+    </div>
+  );
+};
 
-// export default Home;
+export default Home;
