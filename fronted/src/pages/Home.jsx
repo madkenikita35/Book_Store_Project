d01f7fce5c8d08088e79dba2d84c8a4e15131013
Nikita Satish Madke <madkenikita35@gmail.com>
2026-05-03 22:46:10 +0530

    Add Styles

--- fronted/src/pages/Home.jsx
+++ fronted/src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
     >
       <div className=" flex justify-center items-center gap-x-4 ">
         <button
-          className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g"
+          className=" bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g "
           onClick={() => setShowType("table")}
         >
           Table
