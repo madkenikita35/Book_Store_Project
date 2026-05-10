c749450a2061c6f227d5721f3d0b0e116ab7a833
Nikita Satish Madke <madkenikita35@gmail.com>
2026-05-10 23:14:47 +0530

    Add

--- fronted/src/components/home/BooksTable.jsx
+++ fronted/src/components/home/BooksTable.jsx
@@ -34,16 +34,16 @@ const BooksTable = ({ books }) => {
             <td className="border border-slate-700 rounded-md text-center max-md:hidden">
               {book.publishYear}
             </td>
-            <td className=" border border-slate-700 rounded-md text-center">
+            <td className=" border border-slate-700 rounded-md text-center ">
               <div className="flex justify-center gap-x-4">
                 <Link to={`/books/details/${book._id}`}>
-                  <BsInfoCircle className="text-2xl text-green-800 " />
+                  <BsInfoCircle className="text-2xl text-green-800" />
                 </Link>
                 <Link to={`/books/edit/${book._id}`}>
-                  <AiOutlineEdit className="text-2xl text-yellow-600 " />
+                  <AiOutlineEdit className="text-2xl text-yellow-600" />
                 </Link>
                 <Link to={`/books/delete/${book._id}`}>
-                  <MdOutlineDelete className=" text-2xl text-red-600 " />
+                  <MdOutlineDelete className=" text-2xl text-red-600" />
                 </Link>
               </div>
             </td>
