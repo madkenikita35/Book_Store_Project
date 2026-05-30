4b79b98ac98e27b4609b4f9df61da0deb3a3c327
Nikita Satish Madke <madkenikita35@gmail.com>
2026-05-30 23:45:01 +0530

    Add

--- fronted/src/components/home/BooksCard.jsx
+++ fronted/src/components/home/BooksCard.jsx
@@ -22,7 +22,7 @@ const BooksCard = ({ books }) => {
             <PiBookOpenTextLight className="text-red-300 text-2xl " />
             <h2 className="my-1">{item.title}</h2>
           </div>
-          <div className=" flex justify-start items-center gap-x-2">
+          <div className=" flex justify-start items-center gap-x-2 ">
             <BiUserCircle className="text-red-300 text-2xl" />
             <h2 className=" my-1">{item.auther}</h2>
           </div>
