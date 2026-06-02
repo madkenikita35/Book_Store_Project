e4708759189b31ae53323bcb6704e839b4bbd13d
Nikita Satish Madke <madkenikita35@gmail.com>
2026-06-02 23:32:27 +0530

    Add

--- fronted/src/components/home/BooksCard.jsx
+++ fronted/src/components/home/BooksCard.jsx
@@ -24,7 +24,7 @@ const BooksCard = ({ books }) => {
           </div>
           <div className=" flex justify-start items-center gap-x-2 ">
             <BiUserCircle className="text-red-300 text-2xl" />
-            <h2 className="my-1">{item.auther}</h2>
+            <h2 className=" my-1">{item.auther}</h2>
           </div>
           <div className=" flex justify-between items-center gap-x-2 mt-4 p-4 ">
             <Link to={`./books/details/${item._id}`}>
