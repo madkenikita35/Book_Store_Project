7c8a6432020fd43b78609c0b17adf61f9f8a6d85
Nikita Satish Madke <madkenikita35@gmail.com>
2026-06-14 23:09:12 +0530

    Add

--- fronted/src/components/home/BooksTable.jsx
+++ fronted/src/components/home/BooksTable.jsx
@@ -40,7 +40,7 @@ const BooksTable = ({ books }) => {
                   <BsInfoCircle className="text-2xl text-green-800 " />
                 </Link>
                 <Link to={`/books/edit/${book._id}`}>
-                  <AiOutlineEdit className="text-2xl text-yellow-600 " />
+                  <AiOutlineEdit className=" text-2xl text-yellow-600 " />
                 </Link>
                 <Link to={`/books/delete/${book._id}`}>
                   <MdOutlineDelete className=" text-2xl text-red-600" />
