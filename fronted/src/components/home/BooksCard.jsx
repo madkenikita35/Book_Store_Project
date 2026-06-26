44eeefe8becf1b96d7c03a596d3ed2d305782b58
Nikita Satish Madke <madkenikita35@gmail.com>
2026-06-26 23:30:19 +0530

    Add

--- fronted/src/components/home/BooksCard.jsx
+++ fronted/src/components/home/BooksCard.jsx
@@ -14,7 +14,7 @@ const BooksCard = ({ books }) => {
           key={item.id}
           className=" border-2 border-gray-500 rounded-lg px-4 m-4 relative hover:shadow-xl "
         >
-          <h2 className=" absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg ">
+          <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg ">
             {item.publishYear}
           </h2>
           <h4 className=" my-2 text-gray-500">{item._id}</h4>
