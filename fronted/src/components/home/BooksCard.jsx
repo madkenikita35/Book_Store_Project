4322c22108af8e3326ca5510f808ad96beeed864
Nikita Satish Madke <madkenikita35@gmail.com>
2026-07-09 22:53:42 +0530

    Add

--- fronted/src/components/home/BooksCard.jsx
+++ fronted/src/components/home/BooksCard.jsx
@@ -8,7 +8,7 @@ import { PiBookOpenTextLight } from "react-icons/pi";
 
 const BooksCard = ({ books }) => {
   return (
-    <div className=" grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
       {books.map((item) => (
         <div
           key={item.id}
